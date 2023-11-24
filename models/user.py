b456d8c4f3e6620b3b0ec52fd27e663bdcfa505a
Mu-Gee <jamesjim908@gmail.com>
2023-11-24 22:43:31 +0300

    re-updated user.py

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 
 
 class User(BaseModel):
