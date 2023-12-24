f6fb6cef320478a25de9bb3bb2162151f913fa5d
david Njoroge <davidndegwa6869@gmail.com>
2023-12-24 12:17:11 +0300

    adding more routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""minimal flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route index"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route /hbnb"""
+    return "HBNB"
+
+@app.route('/test', strict_slashes=False)
+def test():
+    return "Test"
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
